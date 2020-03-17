f6a78f9f3d6086769e08c4a72b9daa140ad4f632
Doni Setiawan <donisetiawan.050@gmail.com>
2020-03-17 14:29:58 +0700

    Reusable route handlers

--- app.js
+++ app.js
@@ -69,6 +69,52 @@ app.get('/error', (req, res, next) => {
   next(new Error('manually triggered error'));
 });
 
+const articles = {
+  'express-tutorial': {
+    title: 'Practical web apps with Express',
+    content: 'Lean how to create web apps with Express',
+  },
+  'node-videos': {
+    title: 'Node.js video tutorials',
+    content: 'Practical Node tips!',
+  },
+};
+
+const loadArticle = (req, res, next) => {
+  // we assume that the /:article placeholder
+  // is present in the path
+  if (!articles[req.params.article]) {
+    return res.status(404).send('No such article!');
+  }
+
+  req.article = articles[req.params.article];
+
+  next();
+};
+
+const requireAdmin = (req, res, next) => {
+  if (req.ip !== '127.0.0.1') {
+    return res.status(403).send('Forbidden');
+  }
+
+  next();
+};
+
+app.param('article', loadArticle);
+app.get('/articles/:article/:action', requireAdmin);
+
+app.get('/articles/:article', (req, res, next) => {
+  res.send(req.article.content);
+});
+
+app.get(
+  '/articles/:article/edit',
+  requireAdmin,
+  (req, res, next) => {
+    res.send(`Editing article ${req.article.title}`);
+  },
+);
+
 configure('development', () => {
   app.use(errorHandler());
 });
