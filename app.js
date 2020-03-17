58ee6b9b2c4c8b07ca71d3d3f14d27b6e1dfc66d
Doni Setiawan <donisetiawan.050@gmail.com>
2020-03-17 10:26:35 +0700

    Creating configurable middleware

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 import express from 'express';
 import logger from 'morgan';
+import session from 'express-session';
 
 const app = express();
 const port = 3000;
 
+app.use(express.static('/public'));
+app.use(
+  session({
+    secret: 'strategic',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: true,
+    },
+  }),
+);
 app.use('/public', express.static(`${__dirname}/public`));
 // each time somebody visits the admin URL
 // log the ip address as well as other details
-app.use('/admin', logger({ immediate: true }));
+app.use(
+  '/admin',
+  logger(
+    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]',
+    {
+      immediate: true,
+    },
+  ),
+);
 app.use('/admin', (req, res, next) => {
   // we should authenticate the user somehow but for this demo
   // just set the 'isAdmin' flag directly
@@ -17,6 +37,7 @@ app.use((req, res) => {
   if (req.isAdmin) {
     res.send('Hello admin!\n');
   } else {
+    console.log('App process id (pid): %s', process.pid);
     res.send('Hello user!\n');
   }
 });
