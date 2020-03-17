5137847e53935a02536bcdd7b8ebde9327c52ebb
Doni Setiawan <donisetiawan.050@gmail.com>
2020-03-17 10:09:46 +0700

    ...

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 import express from 'express';
+import logger from 'morgan';
 
 const app = express();
 const port = 3000;
 
-const restrictAccess = (req, res, next) => {
-  const { ip } = req;
-  console.log(ip);
-  if (ip === '127.0.0.1' || ip === '::1' || /^192\.168\./.test(ip)) {
-    next();
+app.use('/public', express.static(`${__dirname}/public`));
+// each time somebody visits the admin URL
+// log the ip address as well as other details
+app.use('/admin', logger({ immediate: true }));
+app.use('/admin', (req, res, next) => {
+  // we should authenticate the user somehow but for this demo
+  // just set the 'isAdmin' flag directly
+  req.isAdmin = true;
+});
+app.use((req, res) => {
+  if (req.isAdmin) {
+    res.send('Hello admin!\n');
   } else {
-    res.status(403).send('Forbidden!');
+    res.send('Hello user!\n');
   }
-};
-
-app.use(restrictAccess);
-app.use((req, res, next) => {
-  res.send('Hello world');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
